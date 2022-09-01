62150d09c5aebb874baaf7169d0fce493ef8c281
João Pedro Magalhães <joaopsilvamagalhaes@gmail.com>
2022-09-01 11:21:15 -0300

    ownership: functions

---
+++ ownership/src/main.rs
@@ -0,0 +1,16 @@
+fn main() {
+    let s = String::from("Hello");
+
+    takes_ownership(s);
+
+    let x = 5;
+    makes_copy(x);
+}
+
+fn takes_ownership(some_string: String) {
+    println!("{some_string}");
+}
+
+fn makes_copy(some_integer: i32) {
+    println!("{some_integer}");
+}
